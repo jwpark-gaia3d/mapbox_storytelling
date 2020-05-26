7ffd78a20d36262c7575eb868ea12d914f86a945
박진우 <jwpark@gaia3d.com>
2020-05-26 18:15:51 +0900

    항우연 demo 추가

--- example/항우연/config.js
+++ example/항우연/config.js
@@ -1,9 +1,9 @@
 var config = {
-    style: 'mapbox://styles/swat018/ckagg7vzt0nfw1inzuxu3c5p4',
+    style: 'mapbox://styles/swat018/ckanavwp81ocy1ipbumtu4gv9',
     accessToken: 'pk.eyJ1Ijoic3dhdDAxOCIsImEiOiJja2FleDcyZnQwN2dvMnFuZmx3c2pidzd1In0.W_55REjgA4nRr5CfyjO4cA',
     showMarkers: true,
     markerColor: '#3FB1CE',
-    theme: 'dark',
+    theme: 'light',
     alignment: 'left',
  //   title: 'The Title Text of this Story',
  //   subtitle: 'A descriptive and interesting subtitle to draw in the reader',
@@ -11,16 +11,16 @@ var config = {
     footer: 'Copyrights (c)2019 All rights reserved by Gaia3D,Inc.\n',
     chapters: [
         {
-            id: 'slug-style-id',
-            title: '본사',
+            id: 'kari',
+            title: '항공우주연구원',
  //           image: './path/to/image/source.png',
-            description: '서울특별시 금천구 벚꽃로 278, SJ테크노빌 702호\n' +
-                '우) 08511 전화: 82-2-3397-3475 팩스: 82-2-3397-3478',
+            description: '대전광역시 유성구 과학로 169-84 한국항공우주연구원 \n\n' +
+                '대표전화: (042) 860-2114',
             location: {
-                center: { lon: 126.88448, lat: 37.47983 },
-                zoom: 18.18,
+                center: { lon: 127.35493, lat: 36.37698 },
+                zoom: 17.02,
                 pitch: 60.00,
-                bearing: 95.32
+                bearing: -43.58
             },
             onChapterEnter: [
                 // {
@@ -37,16 +37,44 @@ var config = {
             ]
         },
         {
-            id: 'other-identifier',
-            title: '대전 지사',
+            id: 'naro',
+            title: '나로우주센터 우주과학관',
 //            image: './path/to/image/source.png',
-            description: '대전광역시 유성구 테크노3로 65, 한신에스메카 315호 우)34016\n' +
-                '전화: 82-42-330-0400 팩스: 82-42-330-0410',
+            description: '전라남도 고흥군 봉래면 하반로 490\n' +
+                '전화: (061) 830-8700',
             location: {
-                center: { lon: 127.39065, lat: 36.42929 },
-                zoom: 16.52,
-                pitch: 59.00,
-                bearing: 20.00
+                center: { lon: 127.51808, lat: 34.45358 },
+                zoom: 16.57,
+                pitch: 60.00,
+                bearing: -93.41
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'aero',
+            title: '고흥 항공센터',
+//            image: './path/to/image/source.png',
+            description: '전라남도 고흥군 고흥읍 고소리\n',
+            location: {
+                center: { lon: 127.20787, lat: 34.61047 },
+                zoom: 17.16,
+                pitch: 59.50,
+                bearing: -43.37
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'other-identifier',
+            title: '정부대전청사',
+//            image: './path/to/image/source.png',
+            description: '대전광역시 서구 청사로 189 (둔산동)\n',
+            location: {
+                center: { lon: 127.38497, lat: 36.36129 },
+                zoom: 16.75,
+                pitch: 59.50,
+                bearing: 18.39
             },
             onChapterEnter: [],
             onChapterExit: []
